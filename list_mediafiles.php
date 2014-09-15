ed8e414058ebc447b8e9bdad76fac0d9fddf0e99
kvu022 <klaske.van.vuurden@uit.no>
2014-09-15 15:48:55 +0200

    adding php code

---
+++ list_mediafiles.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ Plugin Name: List mediafiles widget
+Plugin URI: https://github.com/klasske/list_mediafiles_widget
+Description: WordPress widget plugin that lists recent media files in a sidebar
+Version: 1.0
+Author: Klaske van Vuurden
+Author URI: https://github.com/klasske/
+Copyright: Klaske van Vuurden
+*/
+
+
+defined('ABSPATH') or die("Don't access directly!");
+
+class List_MediaFiles_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		// widget actual processes
+		parent::__construct(
+			'list_mediafiles_widget', // Base ID
+			__('List Mediafiles', 'text_domain'), // Name
+			array( 'description' => __( 'A Widget to display Mediafiles', 'text_domain' ), ) // Args
+		);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}	
+
+		$args = array(
+		    'post_type' => 'attachment',
+		    'numberposts' => -1,
+		    'post_status' => null,
+		    'post_parent' => null, // any parent
+		    ); 
+		$attachments = get_posts($args);
+		if ($attachments) {
+		    foreach ($attachments as $post) {
+		        setup_postdata($post);
+		        echo '<h3>' . get_the_title($post->ID) . '</h3>';
+		        the_attachment_link($post->ID, false);
+		        the_excerpt();
+		    }
+		}
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'List_MediaFiles_Widget' );
+});
+
